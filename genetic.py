d2d036f19bf3c681e1b4fccaefb6068a4e777f69
AdtSri413 <aditi.srinivasan@gmail.com>
2023-04-03 15:57:49 -0400

    Added parent and survivor selection functions

--- genetic.py
+++ genetic.py
@@ -9,6 +9,13 @@ about the neural networks that the genes represent (will need to be changed if m
 algorithms are used).
 '''
 import numpy as np
+import random
+
+
+XOVER_RATE = 0.9
+MUT_RATE = 0.2
+TOURNAMENT_SIZE = 4
+
 
 class GeneticAlgorithm():
   def __init__(self, popSize, geneSize, mutationRate):
@@ -16,14 +23,79 @@ class GeneticAlgorithm():
     self.mutationRate = mutationRate
     self.population = np.random.normal(size=(popSize,geneSize))
 
+
   def mutate(self, member):
     pass
+
   
-  def parentSelection(self):
-    pass
+  def parentSelection(self, fitness, mating_pool_size, tournament_size):
+    """Tournament selection without replacement"""
+
+    selected_to_mate = []
+
+    # run tournaments until enough mates are selected
+    while len(selected_to_mate) < mating_pool_size:
+        # Select <tournament_size> random individuals (without replacement, so 
+        # each individual will be different)
+        tournament = random.sample(range(0, len(fitness)), tournament_size)
+        best = tournament[0]
+
+        # Find the best individual in the tournament (based on the fitness)
+        for i in tournament:
+            if fitness[i] > fitness[best]:
+                best = i
+        
+        selected_to_mate.append(best)
+    return np.array(selected_to_mate)
   
+
   def crossover(self):
     pass
+
+
+  def survivor_selection(self, current_pop, current_fitness, offspring, offspring_fitness, fitness_mode="max"):
+    """mu+lambda selection"""
+    import pdb; pdb.set_trace()
+
+    population = []
+    fitness = []
+
+    pooled_pop = [i for i in range(current_pop.shape[0] + offspring.shape[0])]
+    pooled_fitness = current_fitness + offspring_fitness
+
+    # Rank population based on fitness
+    if fitness_mode == "min":
+      max = False
+    elif fitness_mode == "max":
+      max = True
+    else:
+      print("invalid fitness mode. Should be either min or max.")
+      exit(1)
+
+    ranked_pop = [x for _, x in sorted(zip(pooled_fitness, pooled_pop), reverse=max)]
+    fitness = sorted(pooled_fitness, reverse=max)[0:len(current_fitness)]
+
+    # Get the best mu individuals where mu is the size of the current population (without offspring)
+    population = np.zeros(current_pop.shape)
+    for i in range(len(ranked_pop[:current_pop.shape[0]])):
+      try:
+        population[i] = current_pop[ranked_pop[i]]
+      except (IndexError):
+        population[i] = offspring[ranked_pop[i] - current_pop.shape[0]]
+    
+    return population, fitness
   
-  def nextGeneration(self, fitnesses):
-    pass
+
+if __name__ == "__main__":
+  # test survivor selection
+
+  a=np.array([[1, 2, 3], [1, 5, 3], [4, 2, 2], [2, 65, 6]])
+  b=np.array([[6, 8, 4], [6, 9, 2], [3, 7, 1]])
+
+  ga = GeneticAlgorithm(4, 3, 0.2)
+  ga.population = a
+
+  a_fit = [2, 4, 1, 4]
+  b_fit = [0, 5, 3]
+
+  print(ga.survivor_selection(a, a_fit, b, b_fit, "min"))
