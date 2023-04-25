d4f99cfd0bd886c33136377075ffd24c131c3920
AdtSri413 <aditi.srinivasan@gmail.com>
2023-04-25 17:38:18 -0400

    added graphing for phases 1-3 and changed the settings

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Main file that defines everything. Runs the GA over multiple game boards and gen
 '''
 import game, nn, genetic, settings
 import pygame as py
+import matplotlib.pyplot as plt
 
 
 def stepGeneration(games, pacmanGA=None, ghostGAs=None, phase=None):
@@ -15,16 +16,21 @@ def stepGeneration(games, pacmanGA=None, ghostGAs=None, phase=None):
     pacmanFitness.append(fitnesses[-1])
     g.reset()
 
+  point = 0
+
   # comment out if too many logs, but useful to verify the ghosts are learning.
   # print("Count of best fitness for generation: ",ghostFitnesses[i].count(0), "out of ", len(ghostFitnesses[i]))
   if phase == 1:
     count = pacmanFitness.count(0) + sum([ghostFitnesses[i].count(0) for i in range(len(ghostFitnesses))])
     print("Number of entities that avoid walls completely:",count, "of", len(games)*(len(ghostFitnesses)+1), "entities.")
+    point = (count*100)/(len(games)*(len(ghostFitnesses)+1))
   if phase == 2:
     print("Number of games the ghost caught pacman and avoided all walls: ",ghostFitnesses[0].count(0), "out of ", len(ghostFitnesses[0]))
+    point = (ghostFitnesses[0].count(0)*100)/len(ghostFitnesses[0])
   if phase == 3:
     count = pacmanFitness.count(0) + sum([ghostFitnesses[i].count(0) for i in range(len(ghostFitnesses))])
     print("Best pacman fitness: ",max(pacmanFitness)," which occurred",  pacmanFitness.count(max(pacmanFitness))," times out of ", len(pacmanFitness))
+    point = (pacmanFitness.count(max(pacmanFitness))*100)/len(pacmanFitness)
   # no phase 4 print rn because I'm not sure what we would want to
   
   # Get new population
@@ -33,6 +39,8 @@ def stepGeneration(games, pacmanGA=None, ghostGAs=None, phase=None):
   if ghostGAs:
     for ghost in range(settings.ghostCount):
       ghostGAs[ghost].nextGeneration(ghostFitnesses[ghost])
+
+  return point
 
 def nextPhase(ga):
   # In between phases, loose the most recent children, so have 1 wasted generation. But allow
@@ -64,17 +72,25 @@ if __name__ == "__main__":
 
   # Phase 1
   print("PHASE 1: teach entities to avoid the walls")
+
+  y_axis = []
   for gen in range(settings.phase1Generations):
     print(gen+1)
 
     # Run the games, evaluate fitness
-    stepGeneration(games, pacmanGA, ghostGAs, phase=1)
+    point = stepGeneration(games, pacmanGA, ghostGAs, phase=1)
+    y_axis.append(point)
     games = games[0:len(pacmanGA.offspring)] # prune games after initial generation
 
     # assign offspring to each game field, so next iteration runs all of them.
     for i in range(len(games)):
       games[i].assignPopToGame(pacmanGA.offspring[i], [ghostGAs[j].offspring[i] for j in range(settings.ghostCount)])
 
+  plt.plot([x for x in range(settings.phase1Generations)], y_axis, 'k.-')
+  plt.title("Percentage of Agents that Successfully Avoided All Walls")
+  plt.xlabel("Generation")
+  plt.ylabel("Percentage that Avoided Walls")
+  plt.show()
 
   # Transition Phase 1 -> Phase 2
   nextPhase(pacmanGA)
@@ -89,17 +105,25 @@ if __name__ == "__main__":
 
   # Phase 2
   print("PHASE 2: Ghosts learn to go after pacman")
+
+  y_axis = []
   for gen in range(settings.phase2Generations):
     print(gen+1)
 
     # Run the games, evaluate fitness
-    stepGeneration(games, ghostGAs=ghostGAs, phase=2)
+    point = stepGeneration(games, ghostGAs=ghostGAs, phase=2)
+    y_axis.append(point)
     games = games[0:len(ghostGAs[0].offspring)]
 
     # assign offspring to each game field, so next iteration runs all of them.
     for i in range(len(games)):
       games[i].assignPopToGame(pacmanGA.offspring[i], [ghostGAs[j].offspring[i] for j in range(settings.ghostCount)])
 
+  plt.plot([x for x in range(settings.phase2Generations)], y_axis, 'k.-')
+  plt.title("Percentage of Ghosts that Successfully Caught Pacman and Avoided All Walls")
+  plt.xlabel("Generation")
+  plt.ylabel("Percentage that Caught Pacman and Avoided Walls")
+  plt.show()
 
   # Transition Phase 2 -> Phase 3
   for g in ghostGAs:
@@ -112,17 +136,25 @@ if __name__ == "__main__":
 
   # Phase 3
   print("PHASE 3: Pacman learns to go avoid ghosts")
+
+  y_axis = []
   for gen in range(settings.phase3Generations):
     print(gen+1)
 
     # Run the games, evaluate fitness
-    stepGeneration(games, pacmanGA=pacmanGA, phase=3)
+    point = stepGeneration(games, pacmanGA=pacmanGA, phase=3)
+    y_axis.append(point)
     games = games[0:len(pacmanGA.offspring)]
 
     # assign offspring to each game field, so next iteration runs all of them.
     for i in range(len(games)):
       games[i].assignPopToGame(pacmanGA.offspring[i], [ghostGAs[j].offspring[i] for j in range(settings.ghostCount)])
 
+  plt.plot([x for x in range(settings.phase3Generations)], y_axis, 'k.-')
+  plt.title("Percentage of Pacman Agents that Have the Maximum Fitness")
+  plt.xlabel("Generation")
+  plt.ylabel("Percentage that Have Maximum Fitness")
+  plt.show()
 
   # Transition Phase 3 -> Phase 4
   nextPhase(pacmanGA)
--- settings.py
+++ settings.py
@@ -13,26 +13,26 @@ colors = {
 }
 
 # GAME-BASED SETTINGS
-phase1Generations = 5 # Training all entities to avoid the walls
-phase2Generations = 5 # Train ghosts to approach a stand-alone pacman
-phase3Generations = 5 # Train pacman to run away from stand-alone ghosts
-phase4Generations = 5 # Train everything all together
+phase1Generations = 100 # Training all entities to avoid the walls
+phase2Generations = 100 # Train ghosts to approach a stand-alone pacman
+phase3Generations = 100 # Train pacman to run away from stand-alone ghosts
+phase4Generations = 100 # Train everything all together
 iterationsPerGen=100
 ghostCount = 1
 boardSize=10
 
 # NEURAL NETWORK SETTINGS
 inputSize = (ghostCount+1)*2 + 1 # relative position of all other entities + bias
-hiddenSize = 10 # arbitrarily chosen
+hiddenSize = 50 # arbitrarily chosen
 outputSize = 4 # 4 possible actions
 
 # NEURAL NET AND GA SETTINGS
 geneSize = inputSize*hiddenSize + hiddenSize*outputSize
 
 # GA SETTINGS
-populationSize = 100
-mating_pool_size = 10
+populationSize = 2000
+mating_pool_size = 1500
 XOVER_RATE = 0.9
 MUT_RATE = 0.2
-TOURNAMENT_SIZE = 4
-R = 0.5 # Amount to mutate by
+TOURNAMENT_SIZE = 300
+R = 0.2 # Amount to mutate by
